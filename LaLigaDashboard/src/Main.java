15ea17b2e64e27f76d765b6c8af793413d383c8c
alex12kotar1 <alex12kotar@gmail.com>
2026-09-22 16:04:39 -0400

    Update Main.java
    Imported from Eclipse IDE - fixing comments and unwanted text

--- LaLigaDashboard/src/Main.java
+++ LaLigaDashboard/src/Main.java
@@ -6,19 +6,20 @@ import com.sun.net.httpserver.SimpleFileServer;
 import com.sun.net.httpserver.SimpleFileServer.OutputLevel;
 
 public class Main {
+    // api keys / codes for football-data.org
     private static final String API_KEY = "your_api_key";
-    private static final String COMPETITION_CODE = "PD"; // La Liga
+    private static final String COMPETITION_CODE = "PD"; // la liga
 
     public static void main(String[] args) {
         ApiClient client = new ApiClient(API_KEY);
         DashboardGenerator generator = new DashboardGenerator(client);
 
-        // 1. Generate the data into the src/ folder
+        // generate dashboard and advanced team stats
         generator.generateDashboardData(COMPETITION_CODE);
         TeamStatsGenerator.generate();
 
-        // 2. Start a local HTTP server directly from Eclipse
-        var server = SimpleFileServer.createFileServer(new InetSocketAddress(8025),
+        // host locally through java
+        var server = SimpleFileServer.createFileServer(new InetSocketAddress(8002),
                 Path.of("src").toAbsolutePath(), OutputLevel.INFO);
 
         System.out.println("Server started! Open http://localhost:8002 in your browser.");
